4e69deddae57c975ff1b0edd0b91d5199bd8305c
Hafeefa <hafeefapc2003@gmail.com>
2024-03-02 23:47:14 +0530

    login and signup

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,8 @@ void main() {
   runApp(MyApp());
 }
 
+bool rememberMe = false;
+
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
@@ -120,7 +122,11 @@ class Homescreen extends StatelessWidget {
                       width: 600,
                       child: ElevatedButton(
                         onPressed: () {
-                          print('');
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                                builder: (context) => SignUpScreen()),
+                          );
                         },
                         style: ElevatedButton.styleFrom(
                           shape: RoundedRectangleBorder(
@@ -130,6 +136,7 @@ class Homescreen extends StatelessWidget {
                         ),
                         child: Text("Join Now",
                             style: TextStyle(
+                              color: Color(0xFF0249A7),
                               fontSize: 20,
                             )),
                       ),
@@ -141,10 +148,10 @@ class Homescreen extends StatelessWidget {
                   TextButton(
                     onPressed: () {
                       // Navigate to the login page
-                      // Navigator.push(
-                      //   context,
-                      //   MaterialPageRoute(builder: (context) => LoginScreen()),
-                      // );
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(builder: (context) => LoginScreen()),
+                      );
                     },
                     child: Text(
                       'Already have an account? Login',
@@ -181,6 +188,302 @@ class Homescreen extends StatelessWidget {
         height: height,
         width: width,
         fit: fit,
+      ),
+    );
+  }
+}
+
+class LoginScreen extends StatefulWidget {
+  const LoginScreen({Key? key}) : super(key: key);
+
+  @override
+  _LoginScreenState createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  bool rememberMe = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        child: Padding(
+          padding: const EdgeInsets.all(30.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: [
+              Text(
+                'Hi Welcome back!',
+                style: TextStyle(
+                  color: Colors.black,
+                  fontSize: 30,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              SizedBox(height: 10),
+              Text(
+                'Hello again, you have been missed',
+                style: TextStyle(fontSize: 16),
+              ),
+              SizedBox(height: 30),
+              TextField(
+                decoration: InputDecoration(
+                  labelText: 'Email address',
+                  hintText: 'Enter your email address',
+                  border: OutlineInputBorder(),
+                ),
+              ),
+              SizedBox(height: 30),
+              TextField(
+                decoration: InputDecoration(
+                  labelText: 'Password',
+                  hintText: 'Enter your password',
+                  suffixIcon: Icon(Icons.visibility),
+                  border: OutlineInputBorder(),
+                ),
+                obscureText: true, // Hide password
+              ),
+              SizedBox(height: 30),
+              Row(
+                children: [
+                  Checkbox(
+                    value: rememberMe,
+                    onChanged: (value) {
+                      setState(() {
+                        rememberMe = value!;
+                      });
+                    },
+                  ),
+                  Text('Remember me'),
+                ],
+              ),
+              SizedBox(height: 30),
+              Padding(
+                padding: EdgeInsets.only(left: 30, right: 30),
+                child: SizedBox(
+                  height: 50,
+                  width: 600,
+                  child: ElevatedButton(
+                    onPressed: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(builder: (context) => SignUpScreen()),
+                      );
+                    },
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: Color(0xFF0249A7),
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(
+                            10), // Adjust the border radius
+                      ),
+                    ),
+                    child: Text("Login",
+                        style: TextStyle(
+                          fontSize: 20,
+                          color: Colors.white,
+                        )),
+                  ),
+                ),
+              ),
+              SizedBox(
+                height: 30,
+              ),
+              TextButton(
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => SignUpScreen()),
+                  );
+                },
+                child: RichText(
+                  text: TextSpan(
+                    text: "Don't have an account",
+                    style: TextStyle(
+                      color: Colors.black,
+                      fontSize: 16,
+                    ),
+                    children: <TextSpan>[
+                      TextSpan(
+                        text: 'Register',
+                        style: TextStyle(
+                          color: Color(0xFF0249A7),
+                          fontSize: 16,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class SignUpScreen extends StatefulWidget {
+  const SignUpScreen({Key? key}) : super(key: key);
+
+  @override
+  _SignUpScreenState createState() => _SignUpScreenState();
+}
+
+class _SignUpScreenState extends State<SignUpScreen> {
+  bool agree = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        child: Padding(
+          padding: const EdgeInsets.all(20.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: [
+              Text(
+                'Create Account',
+                style: TextStyle(
+                  color: Colors.black,
+                  fontSize: 30,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              SizedBox(height: 10),
+              Text(
+                'Start saving money today!',
+                style: TextStyle(fontSize: 16),
+              ),
+              SizedBox(height: 20),
+              TextField(
+                decoration: InputDecoration(
+                  labelText: 'Email address',
+                  hintText: 'Enter your email address',
+                  border: OutlineInputBorder(),
+                ),
+              ),
+              SizedBox(height: 10),
+              Row(
+                children: [
+                  Container(
+                    width: 90,
+                    height: 55,
+                    decoration: BoxDecoration(
+                      border: Border.all(color: Colors.grey), // Border color
+                      borderRadius: BorderRadius.circular(5.0), // Border radius
+                    ),
+                    padding: EdgeInsets.all(8.0), // Padding inside container
+                    child: Text(
+                      '+91',
+                      style: TextStyle(
+                        fontSize: 16,
+                      ),
+                    ),
+                  ),
+                  SizedBox(width: 10),
+                  Expanded(
+                    child: TextField(
+                      decoration: InputDecoration(
+                        labelText: 'Mobile number',
+                        hintText: 'Enter your phone number',
+                        border: OutlineInputBorder(),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+
+              SizedBox(height: 30),
+              TextField(
+                decoration: InputDecoration(
+                  labelText: 'Pass word',
+                  hintText: 'Enter your password',
+                  suffixIcon: Icon(Icons.visibility),
+                  border: OutlineInputBorder(),
+                ),
+                obscureText: true, // Hide password
+              ),
+              SizedBox(height: 50),
+              // Define a boolean variable to track the checkbox state
+
+// Inside the build method:
+              Row(
+                children: [
+                  Checkbox(
+                    value:
+                        agree, // Set the value based on the rememberMe variable
+                    onChanged: (value) {
+                      // Update the rememberMe variable when the checkbox state changes
+                      setState(() {
+                        agree = value!;
+                      });
+                    },
+                  ),
+                  Text('I agree to the terms and conditions'),
+                ],
+              ),
+              SizedBox(height: 30),
+              Padding(
+                padding: EdgeInsets.only(
+                    left: 30, right: 30), // Set the left and right margin to 20
+                child: SizedBox(
+                  height: 50,
+                  width: 600,
+                  child: ElevatedButton(
+                    onPressed: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(builder: (context) => LoginScreen()),
+                      );
+                    },
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: Color(0xFF0249A7),
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(
+                            10), // Adjust the border radius
+                      ),
+                    ),
+                    child: Text("Sign Up",
+                        style: TextStyle(
+                          fontSize: 20,
+                          color: Colors.white,
+                        )),
+                  ),
+                ),
+              ),
+              SizedBox(
+                height: 10,
+              ), // Adjust spacing between the button and the text
+              TextButton(
+                onPressed: () {
+                  // Navigate to the login page
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => LoginScreen()),
+                  );
+                },
+                child: RichText(
+                  text: TextSpan(
+                    text: 'Already have an account? ',
+                    style: TextStyle(
+                      color: Colors.black, // Default text color
+                      fontSize: 16,
+                    ),
+                    children: <TextSpan>[
+                      TextSpan(
+                        text: 'Login',
+                        style: TextStyle(
+                          color: Color(0xFF0249A7), // Blue text color
+                          fontSize: 16,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
